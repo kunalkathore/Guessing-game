f36031edc2453e8a4624e3b1d94baab6c492ac72
kunal kathore <kunalkathore95@gmail.com>
2026-09-22 19:56:05 +0530

    modified:   index.html modified:   javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 let gameResult = document.getElementById("gameResult");
 let userInput = document.getElementById("userInput");
 let randomNumber = Math.ceil(Math.random() * 100);
+
 function checkGuess() {
   let guessedNumber = parseInt(userInput.value);
   if (guessedNumber > randomNumber) {
